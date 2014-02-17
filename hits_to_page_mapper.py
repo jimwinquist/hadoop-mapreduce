4df1854303fdcf592a9f1ad3bec3d4b06d9cc045
jimwinquist <jawinquist@gmail.com>
2014-02-17 10:21:00 -0800

    Editing hits_to_page_mapper.py to include better url parsing in the map phase using the python urlparse module.

--- hits_to_page_mapper.py
+++ hits_to_page_mapper.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 
 import sys
+from urlparse import urlparse
 
 for line in sys.stdin:
     data = line.strip().split(" ")
 
     if len(data) == 10:
-        ip, identity, username, endtime, zone, method, path, querystring, status, size = data
-        print "{0}".format(path.rstrip("/"))
+        ip, identity, username, endtime, zone, method, url, querystring, status, size = data
+        path = urlparse(url)[2]
+        query = urlparse(url)[4]
+        if query:
+            path += '?' + query
+        print "{0}".format(path)
 
