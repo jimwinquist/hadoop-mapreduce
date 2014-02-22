a544f69a381bad1567eb99f2d27ccf81453442b2
jimwinquist <jawinquist@gmail.com>
2014-02-21 22:58:17 -0800

    Editing post_length_mapper.py to use the abs_parent_id as the primary key instead of the post_id

--- post_length_mapper.py
+++ post_length_mapper.py
@@ -8,13 +8,13 @@ reader.next()
 
 for record in reader:
     if len(record) == 19:
-        post_id, title, tagnames, author_id, body, node_type = record[:6]
+        post_id, title, tagnames, author_id, body, node_type, parent_id, abs_parent_id = record[:8]
         if node_type == 'question':
             marker = 'A'
             print "{0}\t{1}\t{2}".format(post_id, marker, str(len(body)))
         elif node_type == 'answer':
             marker = 'B'
-            print "{0}\t{1}\t{2}".format(post_id, marker, str(len(body)))
+            print "{0}\t{1}\t{2}".format(abs_parent_id, marker, str(len(body)))
         else:
             pass
 
