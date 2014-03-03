2e669a35f7ecd2c51d20beaae4f88c38cd6e2aa1
jimwinquist <jawinquist@gmail.com>
2014-03-03 07:42:40 -0800

    Fixing line length to max of 79 characters.

--- post_length_reducer.py
+++ post_length_reducer.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 '''
-We are interested to see if there is a correlation between the length of a post
-and the length of answers.
+We are interested to see if there is a correlation between the length of
+a post and the length of answers.
 
-The goal is to write a mapreduce program that would process forum_node data
-and output the length of the post and the average answer (just answer, not comment)
-length for each post.
+The goal is to write a mapreduce program that would process forum_node
+data and output the length of the post and the average answer (just
+answer, not comment) length for each post.
 
-In the reducer we start by initializing arrays for storing question and answer lengths for a given key.
-Then we read in our mapper (key, value) pairs from standard input, split them by tab and store the values as
-variables. As we loop through the keys, we will append the values to the appropriate array. Then when we
-come across a new key output the question length and average answer length associated with that question.
+In the reducer we start by initializing arrays for storing question and
+answer lengths for a given key. Then we read in our mapper (key, value)
+pairs from standard input, split them by tab and store the values as
+variables. As we loop through the keys, we will append the values to the
+appropriate array. Then when we come across a new key output the
+question length and average answer length associated with that question.
 
-Each line from the mapper will have the following 3 tab delimited fields:
+Each line from the mapper will have these 3 tab delimited fields:
 post_id\tmarker\post_length
 '''
 
@@ -32,7 +34,8 @@ for line in sys.stdin:
         if question is not None:
             if answers:
                 answers = [int(answer) for answer in answers]
-                print "{0}\t{1}\t{2}".format(oldKey, question, str(sum(answers)/len(answers)))
+                print "{0}\t{1}\t{2}".format(oldKey, question, str(
+                    sum(answers)/len(answers)))
             else:
                 print "{0}\t{1}\t{2}".format(oldKey, question, '0')
                 
@@ -51,6 +54,7 @@ if oldKey is not None:
     if question is not None:
         if answers:
             answers = [int(answer) for answer in answers]
-            print "{0}\t{1}\t{2}".format(oldKey, question, str(sum(answers)/len(answers)))
+            print "{0}\t{1}\t{2}".format(oldKey, question, str(
+                sum(answers)/len(answers)))
         else:
             print "{0}\t{1}\t{2}".format(oldKey, question, '0')
