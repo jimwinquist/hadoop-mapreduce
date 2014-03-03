2d2253b2533af4f218138436ad2761f0cc6bdc2e
jimwinquist <jawinquist@gmail.com>
2014-03-03 07:03:57 -0800

    Adding documentation to post_length_reducer.py and cleaning up the code for functionality, readability, and speed.

--- post_length_reducer.py
+++ post_length_reducer.py
@@ -1,52 +1,56 @@
 #!/usr/bin/python
+'''
+We are interested to see if there is a correlation between the length of a post
+and the length of answers.
+
+The goal is to write a mapreduce program that would process forum_node data
+and output the length of the post and the average answer (just answer, not comment)
+length for each post.
+
+In the reducer we start by initializing arrays for storing question and answer lengths for a given key.
+Then we read in our mapper (key, value) pairs from standard input, split them by tab and store the values as
+variables. As we loop through the keys, we will append the values to the appropriate array. Then when we
+come across a new key output the question length and average answer length associated with that question.
+
+Each line from the mapper will have the following 3 tab delimited fields:
+post_id\tmarker\post_length
+'''
 
 import sys
 
 oldKey = None
-question = []
+question = None
 answers = []
-lengthTotal = 0
-countTotal = 0
 
 for line in sys.stdin:
     record = line.strip().split("\t")
     thisKey = record[0]
     marker = record[1]
+    post_length = record[2]
 
-    if oldKey and oldKey != thisKey:
-        if answers:
-            for answer in answers:
-                countTotal += 1
-                lengthTotal += float(answer)
-        if question:
-            for q in question:
-                if countTotal > 0:
-                    print "{0}\t{1}\t{2}".format(oldKey, q, str(lengthTotal/countTotal))
-                else:
-                    print "{0}\t{1}\t{2}".format(oldKey, q, '0')
+    if oldKey is not None and oldKey != thisKey:
+        if question is not None:
+            if answers:
+                answers = [int(answer) for answer in answers]
+                print "{0}\t{1}\t{2}".format(oldKey, question, str(sum(answers)/len(answers)))
+            else:
+                print "{0}\t{1}\t{2}".format(oldKey, question, '0')
                 
         oldKey = thisKey
-        question = []
+        question = None
         answers = []
-        lengthTotal = 0
-        countTotal = 0
+
 
     oldKey = thisKey
     if marker == 'A':
-        post_length = record[2]
-        question.append(post_length)
+        question = post_length
     elif marker == 'B':
-        answer_length = record[2]
-        answers.append(answer_length)
+        answers.append(post_length)
         
-if oldKey != None:
-    if answers:
-        for answer in answers:
-            countTotal += 1
-            lengthTotal += float(answer)
-    if question:
-        for q in question:
-            if countTotal > 0:
-                print "{0}\t{1}\t{2}".format(oldKey, q, str(lengthTotal/countTotal))
-            else:
-                print "{0}\t{1}\t{2}".format(oldKey, q, '0')
+if oldKey is not None:
+    if question is not None:
+        if answers:
+            answers = [int(answer) for answer in answers]
+            print "{0}\t{1}\t{2}".format(oldKey, question, str(sum(answers)/len(answers)))
+        else:
+            print "{0}\t{1}\t{2}".format(oldKey, question, '0')
